0be60b235584328eaf3574a9ce82458b1906f322
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-04-14 14:08:21 -0500

    linked_list_item tests all passed

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -2,7 +2,42 @@ class LinkedListItem
   include Comparable
 
   attr_reader :payload
+  attr_reader :next_item
 
   def initialize(payload)
   	@payload = payload
   end
+
+  def <=> (other)
+
+  	if self.payload.class == other.payload.class
+  		return self.payload <=> other.payload
+  	end
+  	#Option 1: self.payload.class.to_s <=> other.payload.class.to_s
+  	if self.payload.is_a? Symbol
+  		1
+  	elsif self.payload.is_a? Fixnum
+  		-1
+  	else # I am a String
+  		if other.payload.is_a? Symbol
+  			-1
+  		else
+  			1
+  		end
+  	end
+  end
+
+  def === (other)
+  	self.equal? other
+  end
+
+  def next_item=(lli)
+  	raise ArgumentError, "cant set self as next item" if self == lli
+  	@next_item = lli
+  end
+
+  def last?
+  	next_item.nil?
+  end
+
+end
