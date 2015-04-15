709fa458da59c18240d6892d15a4b9593070f33c
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-04-15 14:15:00 -0500

    implemented get and push, can find the size of the linked list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,49 @@
-class LinkedList
+require_relative 'linked_list_item'
 
+class LinkedList
+	attr_reader :size
+	attr_accessor :last
+	attr_reader :ll
+
+	def initialize
+		@size = 0
+		@last
+		#@ll = LinkedList.new
+	end
+
+	def push(data)
+		if @first_item.nil?
+			@first_item = LinkedListItem.new(data)
+			@last_item = @first_item
+		else
+			@last_item.next_item = LinkedListItem.new(data)
+			@last_item = @last_item.next_item
+			@last = @last_item.payload
+		end
+		@size += 1
+	end
+
+	def get(index)
+		raise IndexError if index < 0 || index > @size
+
+		if index == 0
+			@first_item.payload
+		else
+			count = 0
+			current_node = @first_item
+			while count < index
+				current_node = current_node.next_item
+				count += 1
+			end
+			current_node.payload
+		end
+	end
+
+	def to_s
+		if ll.nil?
+			"| |"
+		else
+
+		end
+	end
 end
