95916c173d869ca1fb69da2edcc02cb5e075d7eb
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-04-17 15:58:40 -0500

    delete method is working

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,7 +3,8 @@ require_relative 'linked_list_item'
 class LinkedList
 	attr_accessor :size
 	attr_accessor :last
-	#attr_accessor :ll
+	attr_accessor :payload
+
 
 	def initialize(*payload)
 		@size = 0
@@ -12,11 +13,12 @@ class LinkedList
 	end
 
 	def push(payload)
+		new_item = LinkedListItem.new(payload)
 		if @first_item.nil?
-			@first_item = LinkedListItem.new(payload)
+			@first_item = new_item
 			@last_item = @first_item
 		else
-			@last_item.next_item = LinkedListItem.new(payload)
+			@last_item.next_item = new_item
 			@last_item = @last_item.next_item
 			@last = @last_item.payload
 		end
@@ -37,23 +39,58 @@ class LinkedList
 		end
 	end
 
-	def to_s(*)
-		str = ""
-		if @size == 0
-			"| |"
-		elsif @size == 1
-			"| #{@first_item.payload} |"
-		else
-			(@size - 1).times do |i|
-				str << "#{get(i)}, "
-			end
-				str << "#{@last_item.payload} "
-			return "| #{str}|"
+	#def to_s
+		#str = ""
+		#if @size == 0
+			#{}"| |"
+		#elsif @size == 1
+			#{}"| #{@first_item.payload} |"
+		#else
+			#(@size - 1).times do |i|
+				#str << "#{get(i)}, "
+			#end
+				#str << "#{@last_item.payload} "
+			#return "| #{str}|"
+		#end
+	#end
+
+	def to_s
+		str = "| "
+		current_item = @first_item
+		until current_item.nil?
+			str << current_item.payload
+			punctuation = current_item.last? ? " " : ", "
+			str << punctuation
+			current_item = current_item.next_item
 		end
+		str << "|"
+		str
 	end
+
 
 	def [] (index)
 		get(index)
+	end
+
+	def []=(index, data)
+		current_node = @first_item
+		index.times do
+			current_node = current_node.next_item
+		end
+		current_node.payload = data
+	end
+
+	def delete(index)
+		@size -= 1
+		current_node = @first_item
+		if index > 0
+			(index - 1).times do
+				current_node = current_node.next_item
+			end
+			 current_node.next_item = current_node.next_item.next_item
+		elsif index <= 0
+			@first_item = current_node.next_item
+		end
 	end
 
 end
